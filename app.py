7bb1fe0ddcd1e86a3a53a78dba7ea054edbe300f
martinamado203-sys <martinamado203@gmail.com>
2025-08-30 23:38:50 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,23 @@
+import os
 from flask import Flask
-
 from flask_migrate import Migrate
-
 from api.api import routes
 from database.database import db
 
-
+# Crear la app
 app = Flask(__name__)
 app.config.from_object('config.Config')
 
+# Inicializar base de datos
 db.init_app(app)
 
+# Inicializar migraciones
 migrate = Migrate(app, db)
 
+# Registrar blueprint con prefijo
 app.register_blueprint(routes, url_prefix='/star')
 
+# Ejecutar la app
 if __name__ == '__main__':
-    app.run(port=80, host='0.0.0.0', debug=False)
+    port = int(os.environ.get("PORT", 80))
+    app.run(host='0.0.0.0', port=port, debug=False)
